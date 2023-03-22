287c8508ead7f2c83c0f506a4e431ed70d700872
Long Tran <longtran.se@gmail.com>
2023-03-22 08:26:04 +0700

    remove host from environment

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 const LOCALHOST = "http://localhost:8080";
-const DEPLOYED = "http://retail-mgt-backend.herokuapp.com";
-const HOST = LOCALHOST;
+const DEPLOYED = "https://retail-mgt-backend.herokuapp.com";
+const HOST = DEPLOYED;
 const DEBT_API = HOST + "/api/debts";
 const EMPLOY_API = HOST + "/employees";
 const TRANSACTION_API = HOST + "/api/transactions";
